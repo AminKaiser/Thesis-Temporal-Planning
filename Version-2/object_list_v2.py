a740fcf3c5b1d83489c7a84897f6621a7cd73829
AminKaiser <aminkaiser90@gmail.com>
2022-05-30 21:57:05 +0600

    version2.1

--- Version-2/object_list_v2.py
+++ Version-2/object_list_v2.py
@@ -2,24 +2,26 @@
 stations = ["Dhaka", "Dhaka-Biman-Bandar", "Tongi", "Narsingdi", "Bhairab", "Brahmanbaria", "Akhaura","Cumilla","Laksham","Feni", "Chittagong",
            "Maijdi-Court","Noakhali","Shaestaganj", "Kulaura", "Sylhet", "Joydebpur", "Mymensingh", "Tangail", "Bangabandhu-Setu", "Ishwardi","Bheramara","Chuadanga","Darshana","Jessore","Khulna",
            "Abdulpur","Rajshahi","Amnura","Chanpainawabganj","Santahar","Bogra","Bonapara","Gaibanda","Jaypurhat","Parbatipur",
-           "Dinajpur","Thakurgaon","Panchagar","Ramgpur","Kaunia","Tista","Kurigram","Lalmonir-Hat","Tush Bhandar","Patgram","Burimari" ]
+           "Dinajpur","Thakurgaon","Panchagar","Rangpur","Kaunia","Tista","Kurigram","Lalmonir-Hat","Tush-Bhandar","Patgram","Burimari" ]
 line_list = ['fl1', 'fl2', 'fl3', 'fl4', 'fl5', 'fl6', 'fl7', 'fl8','fl9','fl10','rl1', 'rl2', 'rl3', 'rl4', 'rl5', 'rl6', 'rl7', 'rl8','rl9','rl10','sl1', 'sl2', 'sl3', 'sl4', 'sl5', 'sl6', 'sl7', 'sl8', 'sl9', 'sl10', 'sl11', 'sl12','sl13','sl14',
             'sl15','sl16','sl17','sl18','sl19','sl20','sl21','sl22','sl23','sl24','sl25','sl26','sl27','sl28','sl29','sl30','sl31','sl32','sl33','sl34','sl35','sl36','sl37']
 train_list = [
     ["suborno-express",["Dhaka","Dhaka-Biman-Bandar","Chittagong"]],
     ["truna-express",["Chittagong","Akhaura","Brahmanbaria","Bhairab","Dhaka-Biman-Bandar","Dhaka"]],
     ["bonolota-express",["Dhaka","Dhaka-Biman-Bandar","Rajshahi"]],
-    ["ekota-express",["Panchagar","Tangail", "Bangabandhu-Setu", "Ishwardi","Joydebpur","Dhaka","Dhaka-Biman-Bandar"]],
-    ["drutojan-express",["Dhaka","Tangail", "Bangabandhu-Setu", "Ishwardi","Joydebpur","Panchagar","Dhaka-Biman-Bandar"]],
-    ["chitra-express",["Khulna","Tangail", "Bangabandhu-Setu", "Ishwardi","Joydebpur","Dhaka","Dhaka-Biman-Bandar"]],
-    ["sundorban-express",["Dhaka","Tangail", "Bangabandhu-Setu", "Ishwardi","Joydebpur","Khulna","Dhaka-Biman-Bandar"]],
+    ["ekota-express",["Panchagar","Tangail", "Bangabandhu-Setu", "Ishwardi","Joydebpur","Dhaka-Biman-Bandar","Dhaka"]],
+    ["drutojan-express",["Dhaka","Tangail", "Bangabandhu-Setu", "Ishwardi","Joydebpur","Panchagar","Dhaka-Biman-Bandar","Dhaka"]],
+    ["chitra-express",["Khulna","Tangail", "Bangabandhu-Setu", "Ishwardi","Joydebpur","Dhaka-Biman-Bandar","Dhaka"]],
+    ["sundorban-express",["Dhaka","Tangail", "Bangabandhu-Setu", "Ishwardi","Joydebpur","Khulna","Dhaka-Biman-Bandar","Dhaka"]],
     ["paharika-express",["Chittagong","Akhaura","Kulaura", "Sylhet"]],
     ["upobon-express",["Sylhet","Kulaura","Shaestaganj","Akhaura","Brahmanbaria","Bhairab","Dhaka-Biman-Bandar","Dhaka"]],
     ["titumir-express",["Rajshahi","Abdulpur","Santahah","Jaypurhat"]],
     ["rupsha-express",["Khulna","Jessore","Chuadanga","Bheramara","Abdulpur","Santahar","Parbatipur"]],
     ["kurigram-express",["Santhar","Bogra", "Bonapara", "Gaibanda","Kaunia","Lalmonir-Hat","Burimari"]],
     ["kapataksha-express",["Khulna","Jessore", "Darshana", "Chuadanga","Bheramara","Ishwardi","Abdulpur","Rajshahi"]],
-    ["udayan-express",["Chittagong","Feni", "Laksham", "Cumilla","Akhaura","Shaestagonj","Kulaura","Sylhet"]],
+    ["udayan-express",["Chittagong","Feni", "Laksham", "Cumilla","Akhaura","Shaestaganj","Kulaura","Sylhet"]],
     ["upokul-express",["Dhaka","Dhaka-Biman-Bandar","Narsingdi","Bhairab", "Brahmanbaria", "Akhaura","Cumilla","Maijdi-Court","Noakhali"]],
     
     ]
+
+
